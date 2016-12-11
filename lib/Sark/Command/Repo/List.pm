14f42e07ab9f6d93514daa24badc9e79e68087f7
Ben Roberts <ben@sihnon.net>
2016-12-11 20:27:23 +0000

    Extend `repo list` command to show only enabled/disabled repos

--- lib/Sark/Command/Repo/List.pm
+++ lib/Sark/Command/Repo/List.pm
@@ -4,18 +4,49 @@ use base qw( Sark::Command::Repo );
 use warnings;
 use strict;
 
+use CLI::Framework::Exceptions qw( :all );
+use Log::Log4perl;
+
 use Sark;
 use Sark::Repo;
 
 sub usage_text {
-    "sark repo list";
+    "sark repo list [--enabled|--disabled]";
+}
+
+sub option_spec {
+    (   [   'filter' => hidden => {
+                one_of => [
+                    [   'enabled|e' =>
+                            'limit output to only enabled repositories',
+                        { default => 0, }
+                    ],
+                    [   'disabled|d' =>
+                            'limit output to only disabled repositories',
+                        { default => 0, }
+                    ],
+                ]
+            }
+        ],
+    );
 }
 
 sub run {
-    my ( $opts, @args ) = @_;
-    my $sark = Sark->new;
+    my ( $self, $opts, @args ) = @_;
+    my $sark   = Sark->new;
+    my $logger = Log::Log4perl->get_logger('Sark::Command::Repo::List');
 
-    my @repos = Sark::Repo::list();
+    my @repos;
+
+    if ( $opts->enabled ) {
+        @repos = Sark::Repo->enabled;
+    }
+    elsif ( $opts->{disabled} ) {
+        @repos = Sark::Repo->disabled;
+    }
+    else {
+        @repos = Sark::Repo->list;
+    }
 
     return { lines => \@repos, };
 }
