41730a65ed1a7419bcdf166e0edaeaf7083b985d
Ettore Di Giacinto <mudler@gentoo.org>
2016-12-14 13:12:24 +0100

    Sark::Build adding accessors pointing to Sark::Config

--- lib/Sark/Build.pm
+++ lib/Sark/Build.pm
@@ -1,7 +1,9 @@
 package Sark::Build;
 use Sark;
 use Deeme::Obj -base;
-has [qw( id config engine )];
+has [qw( id engine )];
+
+has "config" => sub { Sark->instance->{config}; };
 
 sub prepare { Sark->instance->emit( "build.prepare", @_ ) }
 
@@ -12,5 +14,9 @@ sub compile { Sark->instance->emit( "build.compile", @_ ) }
 sub publish { Sark->instance->emit( "build.publish", @_ ) }
 
 sub post_clean { Sark->instance->emit( "build.post_clean", @_ ) }
+
+sub engines { my $self = shift; return $self->config->{build}->{engines}; }
+
+sub docker { my $self = shift; return $self->config->{docker}; }
 
 1;
