457afa5040a33f2dcb88c2e3fd815688d98b47e7
Ettore Di Giacinto <mudler@gentoo.org>
2016-12-16 21:32:04 +0100

    Sark::Engine::Docker: prepare

--- lib/Sark/Engine/Docker.pm
+++ lib/Sark/Engine/Docker.pm
@@ -6,28 +6,34 @@ use Log::Log4perl;
 
 has "name"      => "Docker";
 has "logger"    => sub { Log::Log4perl->get_logger('Sark::Engine::Docker'); };
+has "config"    => sub { Sark->instance->{config}->search("docker") };
 has "interface" => sub {
-    Sark::API::Interface::Docker->new(
-        Sark->instance->{config}->{data}->{docker}->{connection} );
+    my $self = shift;
+    Sark::API::Interface::Docker->new( $self->config->{connection} );
 };
 
 sub prepare {
+    my ( $self, $build, @args ) = @_;
 
 }
 
 sub pre_clean {
+    my ( $self, $build, @args ) = @_;
 
 }
 
 sub compile {
+    my ( $self, $build, @args ) = @_;
 
 }
 
 sub publish {
+    my ( $self, $build, @args ) = @_;
 
 }
 
 sub post_clean {
+    my ( $self, $build, @args ) = @_;
 
 }
 
